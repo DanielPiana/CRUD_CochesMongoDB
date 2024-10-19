dc41931dc56436a77eced8a770dabc04b3aebce0
Pepitoelgringo <kxzdanidp@gmail.com>
2024-10-19 10:49:55 +0200

    Ejercicio completado sin reestricciones ni try catch para posibles errores. Estructura importante

--- src/main/java/org/example/crud_coches/CRUD/CRUDCoches.java
+++ src/main/java/org/example/crud_coches/CRUD/CRUDCoches.java
@@ -2,6 +2,10 @@ package org.example.crud_coches.CRUD;
 
 import com.google.gson.Gson;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.bson.Document;
 import org.example.crud_coches.Controller.HelloController;
 import org.example.crud_coches.domain.Coche;
@@ -16,24 +20,51 @@ public class CRUDCoches {
         * Parsearlo a documento, puesto que solo podemos insertar documentos en MongoDB
         * y para isertar el documento necesitamos una colección*/
         Gson gson = new Gson();
+        //Convertimos el objeto a formato json(Deserializar)
         String json = gson.toJson(coche);
+        //Parseamos el json a documento
         Document doc = Document.parse(json);
+        //y lo insertamos en la base de datos con collection
         HelloController.collection.insertOne(doc);
     }
-    public static void listar() {
+    public static void eliminarCoche(Coche coche) {
+        //Usamos findOneAndDelete para buscar uno y borrarlo
+        //Filters.eq son parámetros de busca, el primero es el atributo que buscamos y el segundo el valor
+        HelloController.collection.findOneAndDelete(Filters.eq("matricula",coche.getMatricula()));
+    }
+    public static void modificarCoche(Coche coche) {
+        //Para modificar coche tenemos que usar findOneAndUpdate, y un filtro de búsqueda combinado para poder cambiar
+        //Mas de un atributo, Updates.set tiene el mismo tipo de atributos que Filters.eq (1º atributo y 2º valor)
+        HelloController.collection.findOneAndUpdate(
+                Filters.eq("matricula",coche.getMatricula()),
+                Updates.combine(
+                        Updates.set("marca",coche.getMarca()),
+                        Updates.set("modelo",coche.getModelo()),
+                        Updates.set("tipo",coche.getTipo())
+                ));
+        }
+    public static ObservableList<Coche> listar() {
+        //Para listar hay que conseguir todos los coches de la base de datos y convertilo a ObservableList para poder cargarlo en la tabla
+        //Hacemos un cursor que va a contener documentos
+        ObservableList<Coche> observableList=null;
         MongoCursor<Document> cursor = HelloController.collection.find().iterator();
         Gson gson = new Gson();
+        //Hacemos un arraylist donde vamos a ir guardando los coches
         ArrayList<Coche> lista = new ArrayList<>();
-        try {
+        try { //Mientras cursor encuentre algo entrará al bucle
             while (cursor.hasNext()) {
+                //Convertimos de json a objeto coche con gson
                 Coche coche = gson.fromJson(cursor.next().toJson(), Coche.class);
+                //y lo añadimos en la lista
                 lista.add(coche);
-                //CONVERTIR OBSERVABLE LIST PARA CARGARLO EN LA TABLA
             }
+            //y lo convertimos a Observablelist
+            observableList = FXCollections.observableList(lista);
         } catch (Exception e ) {
             System.out.println(e.getMessage());
         } finally {
             cursor.close();
         }
+        return observableList;
     }
 }
--- src/main/java/org/example/crud_coches/Controller/HelloController.java
+++ src/main/java/org/example/crud_coches/Controller/HelloController.java
@@ -3,78 +3,87 @@ package org.example.crud_coches.Controller;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.bson.Document;
 import org.example.crud_coches.CRUD.CRUDCoches;
 import org.example.crud_coches.Clases.Conexion;
+import org.example.crud_coches.Utilities.Alerts;
 import org.example.crud_coches.domain.Coche;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static org.example.crud_coches.CRUD.CRUDCoches.eliminarCoche;
+import static org.example.crud_coches.CRUD.CRUDCoches.listar;
+
 
 public class HelloController implements Initializable {
     //Creamos MongoCollection para poder insertar documentos
     public static MongoCollection<Document> collection=null;
+
     MongoClient con;
     String[] listaTipos = {"Gasolina","Diesel","Híbrido","Eléctrico"};
 
     @FXML
-    private Button buttonBuscar;
-
+    private TableColumn<Coche,String> columnaMatricula;
     @FXML
-    private Button buttonCancelar;
-
+    private TableColumn<Coche,String> columnaMarca;
+    @FXML
+    private TableColumn<Coche,String> columnaModelo;
+    @FXML
+    private TableColumn<Coche,String> columnaTipo;
+    @FXML
+    private Button buttonLimpiar;
     @FXML
     private Button buttonEliminar;
-
     @FXML
     private Button buttonGuardar;
-
     @FXML
     private Button buttonModificar;
-
-    @FXML
-    private Button buttonNuevo;
-
     @FXML
     private ComboBox<String> cbTipo;
-
     @FXML
-    private TableView<String> tableCoches;
-
+    private TableView<Coche> tableCoches;
     @FXML
     private TextField txtFieldMarca;
-
     @FXML
     private TextField txtFieldMatricula;
-
     @FXML
     private TextField txtFieldModelo;
 
-    public void onButtonBuscarClick() {
-    }
 
     public void onButtonEliminarClick() {
+        Coche coche = tableCoches.getSelectionModel().getSelectedItem();
+        eliminarCoche(coche);
+        cargarTabla();
+        setearTextFieldsVacios();
+        Alerts.alertaGeneral("Coche eliminado correctamente","INFORMATION");
     }
 
     public void onButtonModificarClick() {
-    }
-
-    public void onButtonCancelarClick() {
+        /*Asumo que la matricula es primary key y no debería cambiar asi que creo un coche con la matricula del coche seleccionado
+        * en la tabla y los datos a modificar, Marca,Modelo y Tipo.*/
+        Coche cocheReferencia = tableCoches.getSelectionModel().getSelectedItem();
+        Coche coche = new Coche(cocheReferencia.getMatricula(), txtFieldMarca.getText(),txtFieldModelo.getText(),cbTipo.getValue());
+        CRUDCoches.modificarCoche(coche);
+        cargarTabla();
+        setearTextFieldsVacios();
+        Alerts.alertaGeneral("Coche modificado correctamente","INFORMATION");
     }
 
     public void onButtonGuardarClick() {
-    }
-
-    public void onButtonNuevoClick() {
         Coche coche = new Coche(txtFieldMatricula.getText(),txtFieldMarca.getText(),txtFieldModelo.getText(),cbTipo.getSelectionModel().getSelectedItem());
         CRUDCoches.insertarCoche(coche);
+        cargarTabla();
+        setearTextFieldsVacios();
+        Alerts.alertaGeneral("Coche guardado correctamente","INFORMATION");
+    }
+    public void onButtonLimpiarClick() {
+        setearTextFieldsVacios();
     }
 
     @Override
@@ -88,5 +97,31 @@ public class HelloController implements Initializable {
         //Creamos la coleccion estática para trabajar con ella en CRUDCoches y la asignamos a la variable collection
         database.createCollection("coches");
         collection = database.getCollection("coches");
+        cargarTabla();
+    }
+    public void cargarTabla() {
+        //Cargamos los datos en la tabla
+        columnaMatricula.setCellValueFactory(new PropertyValueFactory<>("matricula"));
+        columnaMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        columnaModelo.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+        columnaTipo.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+        ObservableList<Coche> observableList = listar();
+        tableCoches.setItems(observableList);
+    }
+
+    public void onTableClick() {
+        //Metodo para setear en los textFields y el comboBox lo seleccionado en la tabla
+        Coche coche = tableCoches.getSelectionModel().getSelectedItem();
+        txtFieldMarca.setText(coche.getMarca());
+        txtFieldMatricula.setText(coche.getMatricula());
+        txtFieldModelo.setText(coche.getModelo());
+        cbTipo.setValue(coche.getTipo());
+    }
+    public void setearTextFieldsVacios() {
+        //Metodo para limpiar los textFields y el comboBox
+        txtFieldModelo.setText("");
+        txtFieldMatricula.setText("");
+        txtFieldMarca.setText("");
+        cbTipo.setValue("");
     }
 }
